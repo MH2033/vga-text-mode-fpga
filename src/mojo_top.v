d10eceb1e3b4598a7d1aecb71c10477ececd432d
MH2033 <mh.akbari@yahoo.com>
2020-10-22 13:21:32 +0330

    Remove enable signal from video timing controller

--- src/mojo_top.v
+++ src/mojo_top.v
@@ -16,52 +16,57 @@ module mojo_top(
 
 	wire rst = ~rst_n; // make reset active high
 	wire pixel_clk;
-	wire [31:0]h_pos;
-	wire [31:0]v_pos;
+	wire [10:0]h_pos;
+	wire [10:0]v_pos;
+	
+	reg [2:0]glyph_x;
+	reg [3:0]glyph_y;
+	reg [7:0]char = 8'h43;
+	
+	wire pixel_val;
+	wire [13:0]addr;
 	
 	assign led = 8'b0;
 	
+	assign addr = (char << 7) + glyph_x + (glyph_y << 3);
 	//Generating 25MHz pixel clock for vga timing control unit
 	clk_25MHz instance_name(
     .CLK_IN1(clk),      
     .CLK_OUT1(pixel_clk),     
     .RESET(rst));
 
-	video_timing_controller #(
-		.h_pixels(640),
-		.v_pixels(480),
-		
-		.hf_porch(16),
-		.h_spulse(96),
-		.hb_porch(48),
-		
-		.vf_porch(10),
-		.v_spluse(2),
-		.vb_porch(33),
-		
-		.h_pol(0),
-		.v_pol(0))
-	 vga_driver (
+	video_timing_controller vga_driver (
     .pixel_clk(pixel_clk), 
-    .reset(rst), 
-    .en(1), 
+    .reset(rst),
     .h_sync(h_sync), 
     .v_sync(v_sync), 
     .h_pos(h_pos), 
     .v_pos(v_pos)
     );
 	 
-	 always @(clk) begin 
-		if(h_pos <= 320 && v_pos <= 240) begin
-			r = 1;
-			g = 1;
-			b = 0;
+	 glyph_rom text_rom (
+		.clka(pixel_clk),
+		.addra(addr),
+		.douta(pixel_val)
+		);
+		
+		
+	 always @(posedge pixel_clk) begin
+		glyph_x <= h_pos[2:0];
+		glyph_y <= v_pos[3:0];
+	 end
+	 
+	 always @(posedge pixel_clk) begin 
+		if(pixel_val && h_pos <= 800 && v_pos <= 600) begin
+			r <= 0;
+			g <= 1;
+			b <= 0;
 		end
 		
 		else begin
-			r = 0;
-			g = 0;
-			b = 0;
+			r <= 0;
+			g <= 0;
+			b <= 0;
 		end
 			
 	 end
--- src/video_timing_controller.v
+++ src/video_timing_controller.v
@@ -30,8 +30,8 @@ module video_timing_controller #(
 		parameter v_spluse = 4,
 		parameter vb_porch = 23,
 		
-		parameter [0:0] h_pol = 1'b1,
-		parameter [0:0] v_pol = 1'b1 )
+		parameter h_pol = 1'b1,
+		parameter v_pol = 1'b1 )
 	(
 	input pixel_clk,
 	input reset,
@@ -40,16 +40,20 @@ module video_timing_controller #(
 	output reg h_sync,
 	output reg v_sync,
 	
-	output reg [$clog2(h_pixels) - 1:0] h_pos,
-	output reg [$clog2(v_pixels) - 1:0] v_pos
+	output reg [31:0] h_pos,
+	output reg [31:0] v_pos
 	
     );
+	
+	
+	parameter h_period = h_pixels + hf_porch + h_spulse + hb_porch;
+	parameter v_period = v_pixels + vf_porch + v_spluse + vb_porch;
 
 	initial begin
-		h_pos = 0;
-		v_pos = 0;
-		h_sync = ~h_pol;
-		v_sync = ~v_pol;
+		h_pos <= 0;
+		v_pos <= 0;
+		h_sync <= ~h_pol;
+		v_sync <= ~v_pol;
 	end
 	
 	//setting horizontal and vertical position based on pixel clock
@@ -63,8 +67,7 @@ module video_timing_controller #(
 		end
 		
 		else begin
-			if(en) begin
-				if(h_pos < h_pixels + hf_porch + h_spulse + hb_porch) begin 
+				if(h_pos < h_period - 1) begin 
 					h_pos <= h_pos + 1;
 					
 				end
@@ -73,7 +76,7 @@ module video_timing_controller #(
 				
 					h_pos <= 0;
 					
-					if (v_pos < v_pixels + vf_porch + v_spluse + vb_porch)begin
+					if (v_pos < v_period - 1)begin
 						v_pos <= v_pos + 1;
 						
 					end
@@ -106,14 +109,6 @@ module video_timing_controller #(
 					v_sync <= v_pol;
 					
 				end
-			end
-			
-			else begin
-				h_sync <= 1'bz;
-				v_sync <= 1'bz;
-				h_pos <= 1'bz;
-				v_pos <= 1'bz;
-			end
 		end
 		
 	end
